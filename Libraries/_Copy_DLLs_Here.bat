e8988b9fa634d821d69100c0584f61e72d0f3ebe
GreyHak <479851+GreyHak@users.noreply.github.com>
2021-03-22 18:58:00 -0700

    Update _Copy_DLLs_Here.bat

--- Libraries/_Copy_DLLs_Here.bat
+++ Libraries/_Copy_DLLs_Here.bat
@@ -1,9 +1,14 @@
 @ECHO OFF
 COPY "%ProgramFiles(x86)%\Steam\steamapps\common\Dyson Sphere Program\BepInEx\core\0Harmony.dll" .
 COPY "%ProgramFiles(x86)%\Steam\steamapps\common\Dyson Sphere Program\BepInEx\core\BepInEx.dll" .
-COPY "%ProgramFiles(x86)%\Steam\steamapps\common\Dyson Sphere Program\DSPGAME_Data\Managed\Assembly-CSharp.dll" .
 COPY "%ProgramFiles(x86)%\Steam\steamapps\common\Dyson Sphere Program\DSPGAME_Data\Managed\UnityEngine.UI.dll" .
 COPY "%ProgramFiles(x86)%\Steam\steamapps\common\Dyson Sphere Program\DSPGAME_Data\Managed\UnityEngine.CoreModule.dll" .
+COPY "%ProgramFiles(x86)%\Steam\steamapps\common\Dyson Sphere Program\DSPGAME_Data\Managed\UnityEngine.ParticleSystemModule.dll" .
 
-REM UnityEngine.dll from this post https://discord.com/channels/806549677209944084/806556447885623336/809242935073832970
-REM Direct link: https://cdn.discordapp.com/attachments/806556447885623336/809242933433598002/UnityEngine.refstub.dll
+ECHO Assembly-CSharp.dll is obtained from https://discord.com/channels/806549677209944084/806556447885623336/809241758323572766
+ECHO Direct link: https://drive.google.com/file/d/1vhvu9yr6NiVFDpKb9Iou8iQVkC4lrLXQ/view?usp=sharing
+
+ECHO UnityEngine.dll from this post https://discord.com/channels/806549677209944084/806556447885623336/809242935073832970
+ECHO Direct link: https://cdn.discordapp.com/attachments/806556447885623336/809242933433598002/UnityEngine.refstub.dll
+
+PAUSE
